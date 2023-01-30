4fb151413598f2d67bccd09842c67dce815a0f0f
m-triple-m <triplem656@mail.com>
2023-01-30 20:01:38 +0530

    ..

--- frontend/src/components/user/ResponseManage.js
+++ frontend/src/components/user/ResponseManage.js
@@ -1,8 +1,11 @@
+import { Divider, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { Formik } from "formik";
 import React, { useEffect, useState } from "react";
 
 const ResponseManage = ({ formid }) => {
   const [responseList, setResponseList] = useState([]);
+  const [emailList, setEmailList] = useState([]);
+  const [selResponse, setSelResponse] = useState(null);
 
   const fetchResponses = async () => {
     const res = await fetch(
@@ -32,9 +35,35 @@ const ResponseManage = ({ formid }) => {
   return (
     <div>
       <div className="section">
-        <div className="container">
-          <div className="row"></div>
-        </div>
+        <FormControl fullWidth>
+          <InputLabel id="emails">Select Response</InputLabel>
+          <Select
+            labelId="emails"
+            id="demo-simple-select"
+            value={selResponse}
+            label="Age"
+            onChange={(e) => setSelResponse(e.target.value)}
+          >
+            {responseList.map((response, index) => (
+              <MenuItem value={index}>{index + 1}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <Divider />
+        {selResponse && (
+          <div>
+            <div className="row">
+              <div className="col-md-4">
+                <p>Email : </p>
+                <p>Form ID : </p>
+              </div>
+            </div>
+            <h1>{responseList[selResponse].email}</h1>
+            <h2>{responseList[selResponse].email}</h2>
+            <h3>{responseList[selResponse].message}</h3>
+          </div>
+
+            )}
       </div>
     </div>
   );
