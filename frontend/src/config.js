d32da0144d79e320d6dd73e93b2c71305dbfba78
R27V <renuverma29jan@gmail.com>
2023-02-07 14:49:49 +0530

    frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,7 @@
 const app_config = {
-    api_url : 'http://localhost:5000'
+    // api_url : 'http://localhost:5000'
+    api_url : 'https://backend-tybr.onrender.com'
+
 }
 
 export default app_config;
